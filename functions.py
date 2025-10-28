228831491bf275760f6a064436de66b2127cfc68
hr19213 <Shoichi19213@gmail.com>
2025-10-28 19:44:18 +0900

    10/28 時間所得関数一応完成 キーボードからの入力を作成予定

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from functools import partial
+from functools import partial as pt
 from datetime import datetime
 import os
 import global_val as g
@@ -71,10 +71,14 @@ class ChanTimeWin(tk.Frame):
         self.rst = tk.Button(self.master)    # 初期化ボタン
         self.ook = tk.Button(self.master)    # 決定ボタン
         self.ccl = tk.Button(self.master)    # 取消ボタン
+        self.csr = tk.Label(self.master)     # カーソル
+        self.plc = 0                         # カーソル位置
+        self.tim.set_txt("10:10:10.10")
+        self.txt = self.tim.out_txt()
 
         # ウインドウの定義
         self.master.title(lg.mwn)            # ウインドウタイトル
-        self.master.geometry("400x300")      # ウインドウサイズ
+        self.master.geometry("400x240")      # ウインドウサイズ
         self.master.resizable(False, False)  # サイズ変更禁止
         self.widgets()
         self.event()
@@ -83,49 +87,108 @@ class ChanTimeWin(tk.Frame):
     def widgets(self):
         # 設定
         self.dsp.configure(text=self.tim.out_txt(), font=("", 60))
-        self.now.configure(text=lg.now, width=10)
-        self.rst.configure(text=lg.rst, width=10)
-        self.ook.configure(text=lg.ook, width=10)
-        self.ccl.configure(text=lg.ccl, width=10)
-
-        # 関数割り当て
-
+        self.now.configure(text=lg.now, width=10, command=self.ps_nw)
+        self.rst.configure(text=lg.rst, width=10, command=self.ps_rs)
+        self.ook.configure(text=lg.ook, width=10, command=self.ps_ok)
+        self.ccl.configure(text=lg.ccl, width=10, command=self.ps_cn)
+        self.csr.configure(bg="black", width=1, font=("", 40))
 
         # 配置
-        self.dsp.place(x=15, y=80)
-        self.now.place(x=120, y=210)
-        self.rst.place(x=210, y=210)
-        self.ook.place(x=210, y=250)
-        self.ccl.place(x=300, y=250)
+        self.dsp.place(x=15, y=20)
+        self.now.place(x=120, y=150)
+        self.rst.place(x=210, y=150)
+        self.ook.place(x=210, y=190)
+        self.ccl.place(x=300, y=190)
+        # self.csr.place(x=22, y=100, height=2)
 
     # イベント
     def event(self):
+        self.master.bind("<KeyPress>", self.ps_ky)
         self.dsp.bind("<ButtonPress>", self.ck_dp)
         # self.dsp.bind("<Motion>", self.ck_dp)
 
     # 文字クリック
     def ck_dp(self, e):
-        print(e.x, e.y)
-        if e.num == 1:
-            if e.x < 90:
-                self.tim.set_int(self.tim.n + 360000)
-            elif e.x < 187:
-                self.tim.set_int(self.tim.n + 6000)
-            elif e.x < 282:
-                self.tim.set_int(self.tim.n + 100)
-            else:
-                self.tim.set_int(self.tim.n + 1)
-        elif e.num == 3:
-            if e.x < 90:
-                self.tim.set_int(self.tim.n - 360000)
-            elif e.x < 187:
-                self.tim.set_int(self.tim.n - 6000)
-            elif e.x < 282:
-                self.tim.set_int(self.tim.n - 100)
-            else:
-                self.tim.set_int(self.tim.n - 1)
-        print(self.tim.n)
-        self.dsp.configure(text=self.tim.out_txt())
+        if e.num == 1:                                             # 左クリック
+            if e.x < 90:                                           # hh
+                k = int(self.txt[0]) * 10 + int(self.txt[1]) + 1   # インクリメント
+                if k >= 100:                                       # 100時間を超えた場合
+                    k = 0                                          # 0時間
+                self.txt = str(k).zfill(2) + self.txt[2:]          # 文字列に戻す
+            elif e.x < 187:                                        # mm
+                k = int(self.txt[3]) * 10 + int(self.txt[4]) + 1   # インクリメント
+                if k >= 60:                                        # 60分を超えた場合
+                    k = 0                                          # 0分
+                self.txt = self.txt[:3] + str(k).zfill(2) + self.txt[5:]  # 文字列化
+            elif e.x < 282:                                        # ss
+                k = int(self.txt[6]) * 10 + int(self.txt[7]) + 1   # インクリメント
+                if k >= 60:                                        # 60秒を超えた場合
+                    k = 0                                          # 0秒
+                self.txt = self.txt[:6] + str(k).zfill(2) + self.txt[8:]  # 文字列化
+            else:                                                  # ms
+                k = int(self.txt[9]) * 10 + int(self.txt[10]) + 1  # インクリメント
+                if k >= 100:                                       # 1秒を超えた場合
+                    k = 0                                          # 0秒
+                self.txt = self.txt[:9] + str(k).zfill(2)          # 文字列化
+        elif e.num == 3:                                           # 右クリック
+            if e.x < 90:                                           # hh
+                k = int(self.txt[0]) * 10 + int(self.txt[1]) - 1   # デクリメント
+                if k < 0:                                          # 0時間未満の場合
+                    k = 99                                         # 99時間
+                self.txt = str(k).zfill(2) + self.txt[2:]          # 文字列に戻す
+            elif e.x < 187:                                        # mm
+                k = int(self.txt[3]) * 10 + int(self.txt[4]) - 1   # デクリメント
+                if k < 0:                                          # 0分未満の場合
+                    k = 59                                         # 59分
+                self.txt = self.txt[:3] + str(k).zfill(2) + self.txt[5:]  # 文字列化
+            elif e.x < 282:                                        # ss
+                k = int(self.txt[6]) * 10 + int(self.txt[7]) - 1   # デクリメント
+                if k < 0:                                          # 0秒未満の場合
+                    k = 59                                         # 59秒
+                self.txt = self.txt[:6] + str(k).zfill(2) + self.txt[8:]  # 文字列化
+            else:                                                  # ms
+                k = int(self.txt[9]) * 10 + int(self.txt[10]) - 1  # デクリメント
+                if k < 0:                                          # 0秒未満の場合
+                    k = 99                                         # 0.99秒
+                self.txt = self.txt[:9] + str(k).zfill(2)          # 文字列化
+        self.dsp.configure(text=self.txt)
+
+    # キーボード押下
+    def ps_ky(self, e):
+        if e.keysym == "Right":
+            self.mv_cs(self.plc + 1)
+        elif e.keysym == "Left":
+            self.mv_cs(self.plc - 1)
+
+    # カーソル移動
+    def mv_cs(self, m):
+        n = m % 8
+        l = [22, 60, 118, 156, 214, 252, 310, 348]
+        self.csr.place(x=l[n], y=100, height=2)
+        self.plc = n
+
+    # 現在時刻押下
+    def ps_nw(self):
+        t = Time()
+        t.get_now()
+        self.txt = t.out_txt()
+        self.dsp.configure(text=self.txt)
+
+    # 初期化押下
+    def ps_rs(self):
+        self.txt = "00:00:00.00"
+        self.dsp.configure(text=self.txt)
+
+    # 決定押下
+    def ps_ok(self):
+        self.master.quit()
+        self.master.destroy()
+
+    # 取消押下
+    def ps_cn(self):
+        self.tim = None
+        self.master.quit()
+        self.master.destroy()
 
 
 # 時間変更関数
